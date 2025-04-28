e79fcce2b64ccd80e2d0cde9d9d9e9a1361216ee
shakil <ahmadshakil22255@gmail.com>
2025-04-28 13:09:59 +0600

    dashboard update

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -6,14 +6,19 @@ const DashBoard = () => {
   console.log(user);
 
   return (
-    <div>
-      <div className="text-center">
-        <h2 className="text-3xl">Dashboard</h2>
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <div>
+        <h2 className="text-4xl pb-2">Welcome, {user && user.displayName}</h2>
         <div>
           <img className="mx-auto" src={user && user.photoURL}></img>
         </div>
-        <h3 className="text-lg">{user && user.name}</h3>
-        <h3 className="text-lg">{user && user.email}</h3>
+        <h3 className="text-lg pb-2">
+          <span className="font-bold">Name:</span> {user && user.name}
+        </h3>
+        <h3 className="text-lg">
+          <span className="font-bold">Email:</span>
+          {user && user.email}
+        </h3>
       </div>
     </div>
   );
